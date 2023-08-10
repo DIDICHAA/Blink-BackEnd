cd638335984ef6bdf45459bd427bba75fd049e03
Sangjun Lee <lsjmc0224@dgu.edu>
2023-08-10 23:00:10 +0900

    FEAT : mainpage 모델, 뷰셋, 시리얼라이저 뼈대 추가

--- blink/main/views.py
+++ blink/main/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.db.models import Count, Q
+
+
+from .models import MainPost
+from .serializers import MainPostSerializer, MainPostListSerializer
 
 # Create your views here.
+
+#mainpostviewset은 POST기능만 있음.
+class MainPostViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.DestroyModelMixin):
+
+    queryset = MainPost.objects.annotate(
+        comments_cnt = Count("comments"),
+        # like_cnt 해야해요
+        )
+    
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MainPostListSerializer
+        return MainPostSerializer
+    
+    # def get_permission
+
+    # create(self, request):
+
+    # aciton(최신순)
+
+    # action(오래된 순)
+
+# class MainCommentViewSet()
+
+# class MainReplyViewSet()
