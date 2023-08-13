1deeea078303dedf174e32d27fb922c0785332d9
Sangjun Lee <lsjmc0224@dgu.edu>
2023-08-13 20:31:28 +0900

    temp : 구글로그인 오류 수정중

--- accounts/models.py
+++ accounts/models.py
@@ -9,37 +9,44 @@ class UserManager(BaseUserManager):
     for authentication instead of usernames.
     """
 
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email, nickname, password):
         """
         Create and save a User with the given email and password.
         """
         if not email:
             raise ValueError(_('The Email must be set'))
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        if not nickname:
+            raise ValueError(_('The nickname must be set'))
+        user = self.model(
+            email = self.normalize_email(email),
+            nickname = nickname,
+        )
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, email, nickname, password):
         """
         Create and save a SuperUser with the given email and password.
         """
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError(_('Superuser must have is_staff=True.'))
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError(_('Superuser must have is_superuser=True.'))
-        return self.create_user(email, password, **extra_fields)
+        user = self.create_user(
+            email,
+            password=password,
+            nickname=nickname,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractUser):
+    id = models.AutoField(primary_key=True)
     username = None
     nickname = models.CharField(default='', max_length=100, null=False, blank=True)
     email = models.EmailField(unique=True, max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['nickname']
