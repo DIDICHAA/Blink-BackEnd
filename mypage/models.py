4f11c6c9ec56c06417a8cc7c418e0c0d5ade7169
DIDICHAA <jenny615@dongguk.edu>
2023-08-15 01:41:52 +0900

    ISSUE SO MANY

--- mypage/models.py
+++ mypage/models.py
@@ -2,6 +2,19 @@ from django.db import models
 from accounts.models import User
 from main.models import MainComment, MainReply, MainPost
 from community.models import ComComment, ComReply, ComPost
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.conf import settings
+
+class Activity(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
 
 class MyRequest(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='myrequests')
