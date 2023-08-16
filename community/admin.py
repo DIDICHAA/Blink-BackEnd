3495e774aaa954e329828de32504c444f5eecf25
Sangjun Lee <lsjmc0224@dgu.edu>
2023-08-17 01:36:17 +0900

    Refactor : 모델들 admin에 추가

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(ComPost)
+admin.site.register(ComComment)
+admin.site.register(CommunityReaction)
+admin.site.register(ComReply)
+admin.site.register(ComPostMedia)
+admin.site.register(ComCommentMedia)
